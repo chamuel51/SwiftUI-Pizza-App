12e175a09fc36b59c743bb71082f1d0ac7bb4492
Miguel Jimenez <chamuel.developer@gmail.com>
2023-10-02 22:05:58 -0400

    Initial Commit

---
+++ HuliPizza/HuliPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuliPizzaApp.swift
+//  HuliPizza
+//
+//  Created by chamuel castillo on 10/2/23.
+//
+
+import SwiftUI
+
+@main
+struct HuliPizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
